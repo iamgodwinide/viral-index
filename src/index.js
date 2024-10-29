d7ca048a600f3d9ee8a6695e5652900f56fc698d
Godwin Idemudia <godwinide@gmail.com>
2024-10-29 22:36:16 +0100

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config({});
-import connect from './db/config'
+import connect from './db/config.js'
 import cron from "node-cron";
 import updateNewsChannel from "./jobs/news";
 import fetchAndSaveTransactions from "./jobs/transactions";
@@ -9,7 +9,7 @@ import fetchAndSaveTransactions from "./jobs/transactions";
 connect();
 
 // Schedule the cron job to run every 5 minutes
-cron.schedule("*/1 * * * *", ()=> {
+cron.schedule("*/1 * * * *", () => {
     updateNewsChannel();
     fetchAndSaveTransactions();
 });
