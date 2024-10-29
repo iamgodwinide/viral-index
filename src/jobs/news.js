93406a1bb2eb8ab340b70c09f62e360b186892fe
Godwin Idemudia <godwinide@gmail.com>
2024-10-29 22:33:25 +0100

    new

--- src/jobs/news.js
+++ src/jobs/news.js
@@ -5,23 +5,23 @@ import newsData from '../data/news.json';
 
 export default async function updateNewsChannel() {
   try {
-    const totalNews = newsData.length | 1; 
+    const totalNews = newsData.length | 1;
     const totaltransactions = await TransactionModel.countDocuments();
     const lastNews = await NewsModel.findOne().sort({ _id: -1 });
     const newsPhase = Math.floor(totaltransactions / 100) | 1;
 
-    if(newsPhase > totalNews){
-        return;
+    if (newsPhase > totalNews) {
+      return;
     };
 
-    if(lastNews?.title === newsData[newsPhase-1].title){
-        return;
+    if (lastNews?.title === newsData[newsPhase - 1].title) {
+      return;
     }
 
-    if(newsPhase < newsData.length){
-        await NewsModel.create(newsData[newsPhase]);
+    if (newsPhase < newsData.length) {
+      await NewsModel.create(newsData[newsPhase]);
     }
-    
+
     return;
   } catch (error) {
     console.error("Error fetching or saving transactions:", error);
